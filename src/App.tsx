6a63e164ca77cbe96baf78f18e584a725ca4880c
Sergey Metelskiy <smetelskii@eyeconweb.com>
2024-03-27 09:57:32 +0700

    Init app

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import {Button} from "./ui/Button";
+
+function App() {
+  return (
+    <Button>Hello World</Button>
+  );
+}
+
+export default App;
---
+++ src/ui/Button/Button.tsx
@@ -0,0 +1,10 @@
+import {FC, PropsWithChildren} from "react";
+import styles from './Button.module.scss';
+
+export const Button: FC<PropsWithChildren> = ({children}) => {
+  return (
+    <button className={styles.button}>
+      {children}
+    </button>
+  );
+}
